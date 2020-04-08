b9180e95afc0009797a3b1d86dbf97c15671919b
Max Beyer <ZeusAndHisBeard@users.noreply.github.com>
2020-04-07 21:01:39 -0500

    first commit

---
+++ src/test/kotlin/com/zeus/apps/dicetables/DiceTablesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zeus.apps.dicetables
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiceTablesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
